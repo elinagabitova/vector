300090523c24f9b54448a7f254a2de8cb0a5b44a
elinagabitova <elinagabitovaa@gmail.com>
2017-03-13 14:31:31 +0400

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -3,6 +3,17 @@
 
 SCENARIO("vector init", "[init]") {
 	vector_t vector;
+	REQUIRE(vector.size() == 0);
+	REQUIRE(vector.capacity() == 0);
+}
+SCENARIO ("vector with pam", "[init]")
+{
+	vector_t v(3);
 	REQUIRE(vector.size() == 3);
 	REQUIRE(vector.capacity() == 3);
+	
+	for (unsinged int i = 0; i<v.size(); ++i)
+	{
+		REQUIRE(v[i] == 0);
+	}
 }
