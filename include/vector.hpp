64d046b2ab852f89be12f95bc49b456f63e12982
elinagabitova <elinagabitovaa@gmail.com>
2017-03-13 15:21:02 +0500

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -6,15 +6,66 @@ private:
     unsigned int size_;
     unsigned int capacity_;
     int* ptr_;
-    auto swap(vector_t& other) -> void;
+    void swap(vector_t& other)
 public:
-    vector_t() noexcept;
-    vector_t(unsigned int size);
-    vector_t(const vector_t& other);
-    auto operator=(const vector_t& other) -> vector_t&;
-    ~vector_t() noexcept;
-    auto size() const noexcept -> unsigned int;
-    auto capacity() const noexcept -> unsigned int;
-    auto push_back(int value) -> void;
-    auto operator[](unsigned int index) const noexcept -> int;
+    vector_t()
+    {
+    };
+    vector_t(unsigned int size)
+        {
+			cout << "enter the size of matrix";
+			cin >> size_;
+		};
+    vector_t(const vector_t& other)
+       {
+			size_ = other.size_;
+			capacity = other.capacity;
+			ptr = new int [capacity];
+
+			for (int i = 0; i < size_; i++)
+			{
+				ptr[i] = new int [other.capacity];
+			}
+		}
+ 
+    vector& operator=(const vector_t& other)		
+        if (this != &other)
+			{
+
+				for (int i = 0; i < size_; i++)
+				{
+					delete[]ptr;
+					size_ = other.size_;
+					capacity = other.capacity;
+
+					ptr = new int*[capacity];
+
+					for (int i = 0; i < capacity; i++)
+					{
+						ptr[i] = new int[capacity];
+					}
+				}
+				ptr[capacity] = other.ptr[capacity];
+			}
+			return *this;
+        }
+    ~vector_t() 
+    {
+        
+    };
+    unsigned int size() const 
+    {
+        return size_;
+    }
+    unsigned int capacity() const
+    {
+        return capacity_;
+    }    
+    void push_back(int value) 
+    {
+        
+    }
+    int operator[](unsigned int index) const 
+    {
+    }
 };
