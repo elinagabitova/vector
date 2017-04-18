13a5ac961da923bbe4accf476275d35358fd4e94
elinagabitova <elinagabitovaa@gmail.com>
2017-04-18 15:03:07 +0400

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -1,6 +1,6 @@
 #include "vector.hpp"
 
-vector_t::vector_t()
+vector_t::vector_t() noexpect
 
 {
 size_ = 0;
@@ -26,153 +26,48 @@ vector_t::vector_t() (const vector_t& other)
 	{
 		ptr_[i] = other.ptr_[i];
 	}
-}
-
-double complex_t::a_() const
-
+auto vector_t:: operator=(const vector_t& other) -> vector_t&
 {
-	return a;
-}
-double complex_t::b_() const
-
-{
-  return b;
-}
-
-bool complex_t::operator == (const complex_t& v) const
-
-{
-	if ((a == v.a) && (b == v.b))
-		return true;
-}
-
-
-complex_t complex_t::operator = (const complex_t& res)
-
-{
-
-	if (this != &res) 
-
+if (this!= &other)
 	{
-
-	a = res.a;
-
-	b = res.b;
-
-	return *this;
-
+	delete [] ptr_;
+	size_ = other.size_;
+	capacity_ = other.capacity_;
+	ptr_ = new int [capacity_];
+	for (int i = 0; i<size;i++)
+		{
+			ptr_[i] = other.ptr_[i];
+		}
 	}
-
-
-
 }
-istream& operator >> (istream&in, complex_t& res) 
-
-{
-
-	cout << "enter real part:" << endl;
-
-	in >> res.a;
-
-	cout << "enter imaginary part:" << endl;
-	in >> res.b;
-	cout << endl;
-	return in;
-
-}
-
-
-
-ostream& operator << (ostream&out,const complex_t& res) 
-
-{
-
-	if (res.b < 0)
-
-	{
-    out << res.a << res.b << "i" << endl;
-	}
-
-	else
-
-		out << res.a << "+" << res.b << "i" << endl;
-
-	return out;
-
-}
-
-complex_t complex_t::operator * (const complex_t& v) const
-
-{
-
-	return complex_t(a*v.a - b*v.b, a*v.b + v.a*b);
-
-}
-
-
-
-complex_t complex_t::operator / (const complex_t& v) const
-
-{
-
-	return complex_t((a*v.a + b*v.b) / (v.a*v.a + v.b*v.b), (v.a*b - a*v.b) / (v.a*v.a + v.b*v.b));
-
-}
-
-
-
-complex_t complex_t::operator += (const complex_t& v) 
-
-{
-
-	a += v.a;
-
-	b += v.b;
-
-	return *this;
-
-}
-
-
-
-complex_t complex_t::operator -= (const complex_t& v) 
-
-{
-
-	a -= v.a;
-
-	b -= v.b;
-
-	return *this;
-
-}
-
-complex_t complex_t::operator /= (const complex_t& v) 
-
-{
-
-	float a2 = (a*v.a + b*v.b) / (v.a*v.a + v.b*v.b);
-
-	b = (v.a*b - a*v.b) / (v.a*v.a + v.b*v.b);
-
-	a = a2;
-
-	return *this;
-
-}
-
-
-
-complex_t complex_t::operator *= (const complex_t& v) 
-
-{
-
-	float a2 = a*v.a - b*v.b;
-
-	b = a*v.b + v.a*b;
-
-	a = a2;
-
-	return *this;
-
-}
+	
+    ~vector_t() noexcept
+    {
+	    size_ = 0;
+	    capacity_ = 0;
+	    delete []ptr_;
+    }
+	
+    auto size() const noexcept -> unsigned int
+    {
+	    return size_;
+    }
+    auto capacity() const noexcept -> unsigned int
+    {
+	    return capacity_;
+    }
+	
+    auto push_back(int value) -> void
+    {
+	    if (capacity ==  0)
+	    {
+		    delete [] ptr_;
+		    capacity = 1; 
+	    }
+	    else if (size_ = 1 >= capacity_)
+	    {
+		    
+	    }
+		
+    }
+    auto operator[](unsigned int index) const noexcept -> int;
